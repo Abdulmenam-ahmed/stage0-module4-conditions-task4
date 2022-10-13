71540220df8725381ccd44098c13beec9b9ae609
Abdulmenam <93938541+Abdulmenam@users.noreply.github.com>
2022-10-13 10:06:48 +0300

    submit the task

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryGreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+    	int max= first;
+    	max= second>= max? second : first ;
+    	
+    	System.out.println(max);
+
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+    	int max= first;
+    	max= second>= max? second : first ;
+    	max= third>=max ? third: max;
+    	
+    	System.out.println(max);
+    
     }
 }
